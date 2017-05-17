9eb346fbd3e0a5f76ff65c17ab034af4be7559c0
jk1dd <portajonk@gmail.com>
2017-05-17 10:16:21 -0600

    add successful api call to the search controller - need to figure out what i have,  get test passing, and then refactor the heck out of it  with service and poro

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
-
+    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location=#{params["q"]}&fuel_type=ELEC,LPG&limit=10&radius=6.0"
+    stuff = JSON.parse(response.body)
+    binding.pry
+    # @search =
   end
 end
