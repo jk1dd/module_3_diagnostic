8046d7892100278d5439fe83602b765621e62e6f
jk1dd <portajonk@gmail.com>
2017-05-17 10:57:25 -0600

    get test back to green with nrel service obj and station obj

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,12 +1,35 @@
 class Station
-  def initalize(zip)
-    @zip = zip
+  def initialize(attrs = {})
+    # @zip = zip
+    @attrs = attrs
   end
 
-  def self.stations
-    NrelService.new(zip).find_stations
+  def self.stations(zip)
+    NrelService.new(zip).find_stations[:fuel_stations].map do |station|
+      Station.new(station)
+    end
+  end
+
+  def station_name
+    attrs[:station_name]
+  end
+
+  def street_address
+    attrs[:street_address]
+  end
+
+  def fuel_type_code
+    attrs[:fuel_type_code]
+  end
+
+  def distance
+    attrs[:distance]
+  end
+
+  def access_days_time
+    attrs[:access_days_time]
   end
 
   private
-  attr_reader :zip
+  attr_reader :attrs
 end
