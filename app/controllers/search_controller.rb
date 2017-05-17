81898cfa2cbb3f105c3d9e081197efba88ab86e5
jk1dd <portajonk@gmail.com>
2017-05-17 11:01:06 -0600

    clean out commented lines, delete cassette to try with a new call, test still green

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location=#{params["q"]}&fuel_type=ELEC,LPG&limit=10&radius=6.0"
-    # stuff = JSON.parse(response.body)
-    # @stations = stuff["fuel_stations"]
     @stations = Station.stations(params["q"])
-    # binding.pry
   end
 end
--- spec/features/user_can_locate_stations_by_zip_spec.rb
+++ spec/features/user_can_locate_stations_by_zip_spec.rb
@@ -1,27 +1,14 @@
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80203
-# And I click "Locate"
-# Then I should be on page "/search" with parameters visible in the url
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-
-# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=ENV[nrel_api_key]&location=80203&fuel_type=ELEC,LPG&limit=10&radius=6.0
-
 require 'rails_helper'
 
 feature 'User can locate stations' do
   context 'by entering zip' do
     scenario 'she can see 10 closest within 6 miles, only electric and propane' do
       VCR.use_cassette('user_locates') do
-        # Capybara.app = AltFuelFinder::Application
         visit '/'
-        # save_and_open_page
 
         fill_in "q", with: "80203"
         click_on "Locate"
-# save_and_open_page
+
         expect(current_path).to eq('/search')
         expect(page).to have_content("Name: UDR")
         expect(page).to have_content("Address: 800 Acoma St")
